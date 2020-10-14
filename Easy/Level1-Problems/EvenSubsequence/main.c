e63578704e8d2e34d3804dc998ff9636cc33cfb2
Lungu Andrei-Sebastian <47700149+MrAndreiL@users.noreply.github.com>
2020-10-14 16:25:33 +0300

    Update main.c

--- Easy/Level1-Problems/EvenSubsequence/main.c
+++ Easy/Level1-Problems/EvenSubsequence/main.c
@@ -11,34 +11,34 @@
 
 int main (void)
 {
-  unsigned int n;
-  std::cin >> n;
+   unsigned int n;
+   std::cin >> n;
 
-  unsigned int lungmax, lungindex, lung, index;
-  float x, y;
-  std::cin >> x;
-  lungmax = lungindex = lung = index = 1;
+   unsigned int lungmax, lungindex, lung, index;
+   float x, y;
+   std::cin >> x;
+   lungmax = lungindex = lung = index = 1;
   
-  for (size_t i = 2; i <= n; i++) {
-    std::cin >> y;
-    if (y == x)
-      lung++;
-    else {
-      if (lung > lungmax) {
-	lungmax   = lung;
-	lungindex = index; 
-      }
-      lung  = 1;
-      index = i; 
-    }
-    x = y;
+   for (size_t i = 2; i <= n; i++) {
+     std::cin >> y;
+     if (y == x)
+       lung++;
+     else {
+       if (lung > lungmax) {
+	 lungmax   = lung;
+	 lungindex = index; 
+       }
+       lung  = 1;
+       index = i; 
+     }
+     x = y;
   }
-  if (lung > lungmax) {
-    lungmax   = lung;
-    lungindex = index;
-  }
+   if (lung > lungmax) {
+     lungmax   = lung;
+     lungindex = index;
+   }
 
-  std::cout << lungmax << " " << lungindex << "\n";
+   std::cout << lungmax << " " << lungindex << "\n";
   
-  return 0;
+   return 0;
 }
