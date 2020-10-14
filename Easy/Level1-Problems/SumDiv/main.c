6ab6a888c46c3ee828b6e102a9373fcd562d595a
MrAndreiL <lunguandrei759@gmail.com>
2020-10-14 22:11:51 +0300

    Adding more problems

---
+++ Easy/Level1-Problems/SumDiv/main.c
@@ -0,0 +1,34 @@
+/*
+   Print the sum of the divisors of a given n. (Given by input through standar input).
+   Example: input  -> 12
+   			output -> 28
+*/
+
+#include <stdio.h>
+
+static unsigned int SumDiv (unsigned int n)
+{
+	unsigned int sum = 0;
+	unsigned int d   = 1;
+
+	while (d * d < n) {
+		if (n % d == 0) 
+			sum += (d + (n / d));
+		d++;
+	}  
+	if (d * d == n)
+		sum += d;
+
+	return sum;
+}
+
+int main (void) 
+{
+	unsigned int n;
+
+	scanf("%u", &n);
+
+	printf("%u\n", SumDiv(n));
+
+	return 0;
+}
